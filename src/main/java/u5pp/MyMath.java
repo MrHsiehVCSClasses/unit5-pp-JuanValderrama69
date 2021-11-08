c5d8f620619fe2e6ef27c1984e09b3ba33b3f1d5
Juan Valderrama <90728114+JuanValderrama69@users.noreply.github.com>
2021-11-08 22:12:44 +0000

    I

--- src/main/java/u5pp/MyMath.java
+++ src/main/java/u5pp/MyMath.java
@@ -1,1 +1,12 @@
 package u5pp;
+class MyMath
+  {
+    public static int sqr(int val)
+    {
+     return(val*val);
+      }
+      public static double sqr(double val)
+    return (val*val);
+public static 
+        
+    
